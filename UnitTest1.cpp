b7f7582cccdd2f337a130bdea95565723a18c718
zhongzhitadajiu <62337342+zhongzhitadajiu@users.noreply.github.com>
2020-04-29 16:22:46 +0800

    Add files via upload

---
+++ UnitTest1.cpp
@@ -0,0 +1,37 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:		
+		TEST_METHOD(TestMethod1)
+		{
+			int num = 10;
+			int parameternum = 2;
+			int range = 10;
+			int openr = 1;
+		}
+		TEST_METHOD(TestMethod2)
+		{
+			int num = 10;
+			int parameternum = 2;
+			int range = 10;
+			int openr = 2;
+			int brackets = 1;
+			int point = 1;
+		}
+		TEST_METHOD(TestMethod3)
+		{
+			int num = 10;
+			int parameternum = 2;
+			int range = 10;
+			int openr = 2;
+			int brackets = 0;
+			int point = 1;
+		}
+	};
+}
